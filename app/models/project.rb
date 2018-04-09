8689cde18044620d32655a9cedfca235d6c430ae
Rastakilla <rastakilla@lanjdl.ca>
2018-04-09 11:01:07 -0400

    Mathieu: La bar de recherche fonctionne

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  def self.search(search)
+  where("titre LIKE ? OR description LIKE ? OR environementTechnologique LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
+  end
 end
