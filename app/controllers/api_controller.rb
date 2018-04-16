59e008c3e98b33e63d738661f3a1b66bb53069a0
Rastakilla <rastakilla@lanjdl.ca>
2018-04-16 19:49:43 -0400

    Mathieu : Commit final

--- app/controllers/api_controller.rb
+++
@@ -1,7 +0,0 @@
-class ApiController < ApplicationController::Base
-  before_filter :verify_jwt_token
-  def verify_jwt_token
-    head :unauthorized if request.headers['Authorization'].nil? ||
-      !AuthToken.valid?(request:headers['Authorization'].split(' ').last)
-  end
-end
